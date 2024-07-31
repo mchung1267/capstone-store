3074fe4ff2872e76bf53095510f4471f0c1033af
mchung1267 <martinc126742@gmail.com>
2024-07-31 16:12:06 +0900

    Add files via upload

---
+++ application/config/email.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config = array(
+    'useragent' => 'CodeIgniter',
+    'protocol' => 'smtp', // 'mail', 'sendmail', or 'smtp'
+    'smtp_host' => 'ssl://smtp.googlemail.com', 
+    'smtp_port' => 465,
+    'smtp_user' => 'ytsushima713@gmail.com',
+    'smtp_pass' => 'LunaYoha9090',
+    'smtp_timeout' => 5,
+    'validate' => TRUE,
+    'mailtype' => 'html', //plaintext 'text' mails or 'html'
+    'charset' => 'iso-8859-1'
+);
